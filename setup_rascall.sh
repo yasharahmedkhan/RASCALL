aa3624e2b87692774f777aeb2e42be839af91532
yasharahmedkhan <132019157+yasharahmedkhan@users.noreply.github.com>
2024-05-30 15:09:22 -0400

    Add files via upload
    once virtual env is setup, run this to install dependencies

---
+++ setup_rascall.sh
@@ -0,0 +1,19 @@
+#/bin/bash #update according to your PC
+
+# Install dependencies
+pip3 install backports.functools-lru-cache
+pip3 install cycler
+pip3 install enum34
+pip3 install kiwisolver
+pip3 install matplotlib
+pip3 install numpy
+pip3 install pyparsing
+pip3 install Pypubsub
+pip3 install python-dateutil
+pip3 install pytz
+pip3 install scipy
+pip3 install six
+pip3 install subprocess32
+pip3 install typing
+
+echo "All dependencies are installed and the virtual environment is set up."
